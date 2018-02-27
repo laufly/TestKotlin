02904dda374e64872f08ee390d8544edb7b6d108
laufly <578939729@qq.com>
2018-02-27 14:14:07 +0800

    change animator to method

--- app/src/main/java/com/wilddog/testkotlin/activity/AnimatorActivity.kt
+++ app/src/main/java/com/wilddog/testkotlin/activity/AnimatorActivity.kt
@@ -8,18 +8,19 @@ import com.wilddog.testkotlin.R
 import kotlinx.android.synthetic.main.activity_animator.*
 
 class AnimatorActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animator)
+        startAnimator()
+    }
 
-        var retation: ObjectAnimator = ObjectAnimator.ofFloat(iv_animator,"rotation",0f,360f)
-        var transAnimator : ObjectAnimator = ObjectAnimator.ofFloat(iv_animator,"alpha",1.0f,0.5f,0.8f,1f)
-        var set = AnimatorSet()
+    fun startAnimator(){
+        val retation: ObjectAnimator = ObjectAnimator.ofFloat(iv_animator,"rotation",0f,360f)
+        val transAnimator : ObjectAnimator = ObjectAnimator.ofFloat(iv_animator,"alpha",1.0f,0.5f,0.8f,1f)
+        val set = AnimatorSet()
         set.playTogether(retation,transAnimator)
         set.setDuration(5000)
         set.start()
     }
-
 
 }
